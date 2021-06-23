fd6ec73ef43168d8dae4194b70cc0e79afe9468c
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2021-06-23 08:42:40 +0200

    Utilisation librairie ZXING

--- app/src/main/java/com/myconnectech/simpleNFC/MainActivity.java
+++ app/src/main/java/com/myconnectech/simpleNFC/MainActivity.java
@@ -1,5 +1,6 @@
 package com.myconnectech.simpleNFC;
 
+import android.graphics.Bitmap;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Build;
@@ -7,12 +8,17 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 import java.util.Arrays;
 
@@ -47,12 +53,55 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Reader
         }
         return new String(hexChars);
     }
+    private static final int BLACK = 0xFF000000;
+    // https://stackoverflow.com/questions/13247701/how-to-add-a-logo-to-qr-code-in-android
+    // https://skrymerdev.wordpress.com/2012/09/22/qr-code-generation-with-zxing/
 
+    public void addQR(String content) {
+        BitMatrix matrix = null;
+        QRCodeWriter qrWriter; // = new QRCodeWriter();
+        qrWriter = new  QRCodeWriter();
+
+        try {
+            matrix = qrWriter.encode(content, // "https://myConnecTech.fr",
+                    BarcodeFormat.QR_CODE,
+                    300,
+                    300 ,
+                    null);
+            int width = matrix.getWidth();
+            int height = matrix.getHeight();
+            int[] pixels = new int[width * height];
+            // All are 0, or black, by default
+            for (int y = 0; y < height; y++) {
+                int offset = y * width;
+                for (int x = 0; x < width; x++) {
+                    if (matrix.get(x, y)) {
+                        pixels[offset + x] = BLACK;
+                    }
+                }
+            }
+            Bitmap bitmap = Bitmap.createBitmap(width, height,
+                    Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+
+            ImageView iv = findViewById(R.id.imageView);
+            iv.setImageBitmap(bitmap);
+            //return bitmap;
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        //image = MatrixToImageWriter.toBufferedImage(matrix);
+        //Bitmap image = toBitmap(matrix);
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
         nfcAdapter.enableReaderMode(this, this, NfcAdapter.FLAG_READER_NFC_A, null);
+        Log.d("RUN", "onResume-1");
+        addQR("https://myConnecTech.fr");
+        Log.d("RUN", "onResume-2");
     }
 
     @Override
@@ -81,5 +130,7 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Reader
         Log.d("NFC", "onTagDiscovered");
         twID.setText(bytesToHex(tag.getId()));
         tw.setText("discover "+tag.toString());
+
+        addQR(bytesToHex(tag.getId()));
     }
 }
